55d077c1e799d15e896163a87aa6fd3915e4eec1
1938917626@qq.com <1938917626@qq.com>
2019-10-13 17:00:47 +0800

    小更改

--- B2CSite/B2CSite.Master.designer.cs
+++ B2CSite/B2CSite.Master.designer.cs
@@ -33,15 +33,6 @@ namespace B2CSite
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// ContentPlaceHolder1 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        /// <summary>
         /// SiteMapDataSource1 控件。
         /// </summary>
         /// <remarks>
@@ -67,5 +58,14 @@ namespace B2CSite
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.Calendar Calendar1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
